ac33f48ca8ae1d4cd2a20e70af1b580eef2d99f0
ankomahene <ankomaheneshadrack@gmail.com>
2022-09-21 23:06:52 +0000

    added login component

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-const UserLogin = () => {
-  return <div>UserLogin</div>;
+export const UserLogin = ({ handleLogin }) => {
+  const [name, setName] = useState("");
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h4" textAlign="center" gutterBottom>
+          Login in
+        </Typography>
+        <TextField
+          label="username"
+          placeholder="enter username"
+          size="small"
+          style={{ margin: "10px" }}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <Button
+          variant="contained"
+          style={{ margin: "10px" }}
+          onClick={() => handleLogin(name)}
+        >
+          Join chat
+        </Button>
+      </CardContent>
+      <CardActions
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      ></CardActions>
+    </Card>
+  );
 };
-
-export default UserLogin;
